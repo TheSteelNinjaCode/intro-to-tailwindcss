78f4edcb790cc5170e8368a4c99493fa87bc7d24
Jefferson Abraham <abrahamjefferson38@gmail.com>
2023-08-07 02:39:48 -0600

    Style complete

--- src/app/api/users/route.ts
+++ src/app/api/users/route.ts
@@ -3,59 +3,72 @@ import { prisma } from "../../../lib/prisma";
 
 export async function GET() {
   const users = await prisma.user.findMany();
-  return new Response(JSON.stringify({ users, Message: "Response from API" }), {
-    status: 200,
-  });
+  return new Response(
+    JSON.stringify({ users, Message: users.length > 0 ? "Users" : "No users" }),
+    {
+      status: 200,
+    },
+  );
 }
 
 export async function POST(req: Request) {
   const user = (await req.json()) as User;
 
-  const userRegistered = await prisma.user.create({
-    data: user,
-  });
-  return new Response(
-    JSON.stringify({ userRegistered, Message: "Se ha registrado con exito" }),
-    {
-      status: 200,
-    }
-  );
+  try {
+    const userRegistered = await prisma.user.create({
+      data: user,
+    });
+    return new Response(
+      JSON.stringify({ userRegistered, Message: "Se ha registrado con exito" }),
+      {
+        status: 200,
+      },
+    );
+  } catch (error: unknown) {
+    return new Response(JSON.stringify({ error }), {
+      status: 500,
+    });
+  }
 }
 
 export async function PUT(req: Request) {
   const user = (await req.json()) as User;
 
-  const userUpdated = await prisma.user.update({
-    where: { Id: user.Id },
-    data: user,
-  });
-  return new Response(
-    JSON.stringify({ userUpdated, Message: "Se ha actualizado con exito" }),
-    {
-      status: 200,
-    }
-  );
+  try {
+    const userUpdated = await prisma.user.update({
+      where: { id: user.id },
+      data: user,
+    });
+    return new Response(
+      JSON.stringify({ userUpdated, Message: "Se ha actualizado con exito" }),
+      {
+        status: 200,
+      },
+    );
+  } catch (error: unknown) {
+    return new Response(JSON.stringify({ error }), {
+      status: 500,
+    });
+  }
 }
 
 export async function DELETE(req: Request) {
-  console.log("🚀 ~ file: route.ts:42 ~ DELETE ~ req", req);
   const { searchParams } = new URL(req.url);
-  const id = Number(searchParams.get("Id"));
-  console.log("🚀 ~ file: route.ts:44 ~ DELETE ~ id:", id);
+  const id = Number(searchParams.get("id"));
 
-  const postId =
-    typeof id === "string"
-      ? parseInt(id)
-      : Array.isArray(id)
-      ? parseInt(id[0])
-      : id;
-  const userDeleted = await prisma.user.delete({
-    where: { Id: postId },
-  });
-  return new Response(
-    JSON.stringify({ userDeleted, Message: "Se ha eliminado con exito" }),
-    {
-      status: 200,
-    }
-  );
+  try {
+    const userDeleted = await prisma.user.delete({
+      where: { id: id },
+    });
+    return new Response(
+      JSON.stringify({ userDeleted, Message: "Se ha eliminado con exito" }),
+      {
+        status: 200,
+      },
+    );
+  } catch (error: unknown) {
+    return new Response(JSON.stringify({ error }), {
+      status: 500,
+    });
+  }
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 "use client";
 
 import { User } from "@prisma/client";
-import { ChangeEvent, SyntheticEvent, useEffect, useState } from "react";
+import { ChangeEvent, MouseEvent, useEffect, useState } from "react";
 import axios from "axios";
+import { FaPenToSquare, FaTrashCan } from "react-icons/fa6";
 
 export default function Home() {
   const [users, setUsers] = useState<User[]>([]);
+  const [errors, setErrors] = useState<string[]>([]);
   const [user, setUser] = useState<User>({
     id: 0,
     login: "",
@@ -22,6 +24,13 @@ export default function Home() {
 
     if (res && res.data) {
       setUsers(res.data.users);
+      setUser((prevState) => ({
+        ...prevState,
+        id: 0,
+        login: "",
+        email: "",
+        password: "",
+      }));
       console.log("GetUsers->res.data: ", res.data);
     }
   };
@@ -30,62 +39,91 @@ export default function Home() {
     GetUsers();
   }, []);
 
-  const addUser = async (e: SyntheticEvent) => {
+  const AddUser = async (e: MouseEvent<HTMLButtonElement>) => {
+    setErrors([]);
+
+    if (user.login.length < 1) {
+      setErrors((prevState) => [...prevState, "Login can't be empty"]);
+      return;
+    } else if (user.email.length < 1) {
+      setErrors((prevState) => [...prevState, "Email can't be empty"]);
+      return;
+    } else if (user.password.length < 3) {
+      setErrors((prevState) => [
+        ...prevState,
+        "Password must be 3 characters or more",
+      ]);
+      return;
+    }
     e.preventDefault();
 
     const resp = await axios.post("/api/users", {
-      Login: user.login,
-      Email: user.email,
-      Password: user.password,
+      login: user.login,
+      email: user.email,
+      password: user.password,
     });
 
     if (resp && resp.data) {
       console.log("AddUser->resp.data: ", resp.data);
       GetUsers();
     }
-
-    ResetUser();
   };
 
-  const UpdateUser = async (e: SyntheticEvent) => {
+  const UpdateUser = async (e: MouseEvent<HTMLButtonElement>) => {
+    setErrors([]);
+    if (user.login.length < 1) {
+      setErrors((prevState) => [...prevState, "Login cant'n be empty"]);
+      return;
+    } else if (user.email.length < 1) {
+      setErrors((prevState) => [...prevState, "Email cant'n be empty"]);
+      return;
+    } else if (user.password.length < 3) {
+      setErrors((prevState) => [
+        ...prevState,
+        "Password must be 3 characters or more",
+      ]);
+      return;
+    }
     e.preventDefault();
 
     const resp = await axios.put("/api/users/", {
-      Id: user.id,
-      Login: user.login,
-      Email: user.email,
-      Password: user.password,
+      id: user.id,
+      login: user.login,
+      email: user.email,
+      password: user.password,
     });
 
     if (resp && resp.data) {
       console.log("UpdateUser->resp.data: ", resp.data);
       GetUsers();
     }
-
-    ResetUser();
   };
 
   const ResetUser = () => {
     setUser((prevState) => ({
       ...prevState,
-      Id: 0,
-      Login: "",
-      Email: "",
-      Password: "",
+      id: 0,
+      login: "",
+      email: "",
+      password: "",
     }));
   };
 
-  const EditUser = async (userId: number) => {
+  const EditUser = (userId: number) => {
     const userFound = users.find((user) => user.id === userId);
     if (userFound) {
       setUser(userFound);
     }
   };
 
-  const DeleteUser = async (userId: number) => {
+  const DeleteUser = async (userId: number, deleteConfirm: boolean) => {
+    user.id = userId;
+    (window as any).deleteModal.showModal();
+    if (!deleteConfirm) return;
+
     const resp = await axios
       .delete("/api/users", {
-        params: { Id: userId },
+        params: { id: userId },
       })
       .catch((error) => {
         console.log("catch: ", error.message);
@@ -96,69 +134,142 @@ export default function Home() {
     }
   };
 
+  const CancelEdit = () => {
+    ResetUser();
+  };
+
   // Update specific input field
   const HandleChange = (e: ChangeEvent<HTMLInputElement>) =>
     setUser((prevState) => ({ ...prevState, [e.target.name]: e.target.value }));
 
   return (
-    <main>
-      <h1>Users</h1>
-      <form>
-        <input
-          onChange={HandleChange}
-          value={user.login}
-          type="text"
-          name="Login"
-          placeholder="Login"
-        />
-        <br />
-        <input
-          onChange={HandleChange}
-          value={user.email}
-          type="email"
-          name="Email"
-          placeholder="Email"
-        />
-        <br />
-        <input
-          onChange={HandleChange}
-          value={user.password}
-          type="password"
-          name="Password"
-          placeholder="Password"
-        />
-        <br />
-        <div>
-          <button onClick={addUser}>Add User</button>
-          <button onClick={UpdateUser}>Update User</button>
-        </div>
-      </form>
+    <main className="flex h-screen w-screen flex-col items-center justify-center gap-4 ">
+      <h1 className="mb-3 text-4xl font-bold">Users</h1>
+      {/* <div className="card scale-90 border border-gray-200 bg-base-100 p-4 shadow-xl transition duration-300 ease-in-out hover:scale-100 hover:border-gray-700 hover:shadow-2xl"> */}
+      <div className="my-card">
+        <div className="flex gap-4 divide-x-2 divide-dotted">
+          <div className="space-y-4">
+            <ul>
+              {errors.map((error, idx) => (
+                <li className="text-red-500" key={idx}>
+                  {error}
+                </li>
+              ))}
+            </ul>
 
-      <table>
-        <thead>
-          <tr>
-            <th>ID</th>
-            <th>Login</th>
-            <th>Email</th>
-            <th>Password</th>
-            <th>Actions</th>
-          </tr>
-        </thead>
-        <tbody>
-          {users.map((user: User) => (
-            <tr key={user.id}>
-              <td>{user.id}</td>
-              <td>{user.login}</td>
-              <td>{user.email}</td>
-              <td>{user.password}</td>
-              <td>
-                <button onClick={() => EditUser(user.id)}>Editar</button>
-                <button onClick={() => DeleteUser(user.id)}>Delete</button>
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+            <form className="flex flex-col gap-2">
+              <input
+                className="input input-bordered"
+                onChange={HandleChange}
+                value={user.login}
+                type="text"
+                name="login"
+                required
+                placeholder="Login"
+              />
+              <input
+                className="input input-bordered"
+                onChange={HandleChange}
+                value={user.email}
+                type="email"
+                name="email"
+                required
+                placeholder="Email"
+              />
+              <input
+                className="input input-bordered"
+                onChange={HandleChange}
+                value={user.password}
+                type="password"
+                name="password"
+                required
+                placeholder="Password"
+                minLength={3}
+              />
+              <div className="space-x-2">
+                <button
+                  type="submit"
+                  className="btn btn-primary disabled:bg-gray-600 disabled:text-gray-300"
+                  disabled={user.id > 0 ? true : false}
+                  onClick={AddUser}
+                >
+                  Add
+                </button>
+                <button
+                  type="submit"
+                  className="btn btn-accent disabled:bg-gray-600 disabled:text-gray-300"
+                  disabled={user.id < 1 ? true : false}
+                  onClick={UpdateUser}
+                >
+                  Update
+                </button>
+                <button
+                  type="submit"
+                  className="btn btn-primary disabled:bg-gray-600 disabled:text-gray-300"
+                  disabled={user.id < 1 ? true : false}
+                  onClick={CancelEdit}
+                >
+                  Cancel
+                </button>
+              </div>
+            </form>
+          </div>
+
+          <div className="overflow-auto ps-4">
+            <table className="table">
+              <thead>
+                <tr className="table-head">
+                  <th>ID</th>
+                  <th>Login</th>
+                  <th>Email</th>
+                  <th>Actions</th>
+                </tr>
+              </thead>
+              <tbody>
+                {users.map((user: User) => (
+                  <tr key={user.id}>
+                    <td>{user.id}</td>
+                    <td>{user.login}</td>
+                    <td>{user.email}</td>
+                    <td className="space-x-2">
+                      <button
+                        className="btn btn-warning btn-sm"
+                        onClick={() => EditUser(user.id)}
+                      >
+                        <FaPenToSquare />
+                      </button>
+                      <button
+                        className="btn btn-error btn-sm"
+                        onClick={() => DeleteUser(user.id, false)}
+                      >
+                        <FaTrashCan />
+                      </button>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+
+      <dialog id="deleteModal" className="modal">
+        <form method="dialog" className="modal-box">
+          <button className="btn btn-circle btn-ghost btn-sm absolute right-2 top-2">
+            ✕
+          </button>
+          <h3 className="text-lg font-bold">Delete</h3>
+          <p className="py-4">Are you sure you want to delete this user?</p>
+          <div className="modal-action">
+            <button
+              onClick={() => DeleteUser(user.id, true)}
+              className="btn btn-primary"
+            >
+              Yes
+            </button>
+          </div>
+        </form>
+      </dialog>
     </main>
   );
 }
